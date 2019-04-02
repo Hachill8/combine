2626c6940100f15eb9cf6e1a3d4e548fba09a77e
Willy <zx85hy6538@gmail.com>
2019-04-02 23:17:35 +0800

    20190402-修改fab

--- app2/src/main/java/com/example/hy/MainActivity.java
+++ app2/src/main/java/com/example/hy/MainActivity.java
@@ -9,44 +9,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab,fab_ft01,fab_ft02,fab_ft03,fab_ft04,fab_ft05;
-    boolean fg=false;TextView tv;
+    ImageButton fab,fab_ft01,fab_ft02,fab_ft03,fab_ft04;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fab=(FloatingActionButton)findViewById(R.id.fab);
-        fab_ft01=(FloatingActionButton)findViewById(R.id.fab_紀);
-        fab_ft02=(FloatingActionButton)findViewById(R.id.fab_討);
-        fab_ft03=(FloatingActionButton)findViewById(R.id.fab_尋);
-        fab_ft04=(FloatingActionButton)findViewById(R.id.fab_曆);
-        fab_ft05=(FloatingActionButton)findViewById(R.id.fab_商);
-        tv=(TextView)findViewById(R.id.cloud);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(fg==false){
-                    openmenu(view);
-                    fg=true;
-                }else{
-                    closemenu(view);
-                    fg=false;
-                }
-            }
-        });
-
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                closemenu(fab);
-            }
-        });
+        fab=(ImageButton)findViewById(R.id.fab);
+        fab_ft01=(ImageButton)findViewById(R.id.fab_紀);
+        fab_ft02=(ImageButton)findViewById(R.id.fab_討);
+        fab_ft03=(ImageButton)findViewById(R.id.fab_商);
+        fab_ft04=(ImageButton)findViewById(R.id.fab_曆);
 
         fab_ft04.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,36 +41,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        fab_ft03.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,search.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-
-    private void openmenu(View view){
-        fab_ft01.show();fab_ft02.show();fab_ft03.show();fab_ft04.show();fab_ft05.show();
-        ObjectAnimator animator=ObjectAnimator.ofFloat(view,"rotation",0,-155,-135);
-        animator.setDuration(500);
-        animator.start();
-        tv.setVisibility(View.VISIBLE);
-        AlphaAnimation alphaAnimation=new AlphaAnimation(0,0.7f);
-        alphaAnimation.setDuration(500);
-        alphaAnimation.setFillAfter(true);
-        tv.startAnimation(alphaAnimation);
-    }
-
-    private void closemenu(View view){
-        fab_ft01.hide();fab_ft02.hide();fab_ft03.hide();fab_ft04.hide();fab_ft05.hide();
-        ObjectAnimator animator=ObjectAnimator.ofFloat(view,"rotation",-135,20,0);
-        animator.setDuration(500);
-        animator.start();
-        AlphaAnimation alphaAnimation=new AlphaAnimation(0.7f,0);
-        alphaAnimation.setDuration(500);
-        tv.setVisibility(View.GONE);
-        tv.startAnimation(alphaAnimation);
     }
 }
