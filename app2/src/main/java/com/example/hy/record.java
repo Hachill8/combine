583269fc732fb4c61f8716b8d181c8c258fc2c44
Willy <zx85hy6538@gmail.com>
2019-04-04 00:58:48 +0800

    20190404-修改record_bar

--- app2/src/main/java/com/example/hy/record.java
+++ app2/src/main/java/com/example/hy/record.java
@@ -1,14 +1,18 @@
 package com.example.hy;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class record extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
     }
 }
