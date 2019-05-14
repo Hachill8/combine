61ff762f33939ac94e168392c297f35932a522bb
angeltsai <410535036@gms.ndhu.edu.tw>
2019-05-14 16:49:32 +0800

    建立tablayout-3(含button）

--- app2/src/main/java/com/example/hy/forum.java
+++ app2/src/main/java/com/example/hy/forum.java
@@ -7,6 +7,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 public class forum extends AppCompatActivity {
     Toolbar toolbar;
@@ -15,6 +16,7 @@ public class forum extends AppCompatActivity {
     PagerAdapter pagerAdapter;
     TabItem tabDiscussion;
     TabItem tabExchange;
+    Button bt1,bt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,10 @@ public class forum extends AppCompatActivity {
         viewPager = findViewById(R.id.viewpager);
 
         pagerAdapter = new forum_PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount()) ;
-
         viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        bt1=findViewById(R.id.BT1);
+        bt2=findViewById(R.id.BT2);
     }
 }
