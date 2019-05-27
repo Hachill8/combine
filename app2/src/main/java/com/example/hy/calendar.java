20dfe4c5f9e306f49ad4c36dd4f3c3bb6b1cfbff
Hachill8 <410535039@gms.ndhu.edu.tw>
2019-05-27 17:31:39 +0800

    栽培曆更新

--- app2/src/main/java/com/example/hy/calendar.java
+++ app2/src/main/java/com/example/hy/calendar.java
@@ -1,18 +1,26 @@
 package com.example.hy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class calendar extends AppCompatActivity {
     CalendarView cv1;
-    TextView tv1,tv2;
+    TextView tv1,tv2,tv3,tv4,tv5,tv6,tv7;
     Button edit,addact;
+    ImageButton imb1,imb2,imb3,imb4,imb5;
     private static  final  int REQUEST_CODE=1;
 
     @Override
@@ -22,8 +30,18 @@ public class calendar extends AppCompatActivity {
 
         cv1 = (CalendarView)findViewById(R.id.CV01);
         tv1 = (TextView)findViewById(R.id.TV01);
-        tv2 = (TextView)findViewById(R.id.TV2);
+        tv2 = (TextView)findViewById(R.id.TV02);
+        tv3 = (TextView)findViewById(R.id.TV03);
+        tv4 = (TextView)findViewById(R.id.TV04);
+        tv5 = (TextView)findViewById(R.id.TV05);
+        tv6 = (TextView)findViewById(R.id.TV06);
+        tv7 = (TextView)findViewById(R.id.TV07);
         edit =(Button)findViewById(R.id.edit);
+        imb1 =(ImageButton) findViewById(R.id.IMB01);
+        imb2 =(ImageButton) findViewById(R.id.IMB02);
+        imb3 =(ImageButton) findViewById(R.id.IMB03);
+        imb4 =(ImageButton) findViewById(R.id.IMB04);
+        imb5 =(ImageButton) findViewById(R.id.IMB05);
         addact =(Button)findViewById(R.id.addact);
 
         cv1.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -37,7 +55,7 @@ public class calendar extends AppCompatActivity {
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(calendar.this,cal_edittext.class);
+                Intent intent = new Intent(calendar.this,calendar_memo.class);
                 startActivity(intent);
             }
         });
@@ -54,5 +72,132 @@ public class calendar extends AppCompatActivity {
         //把佈局檔案中的文字框和textview連結起來
         //在textview中顯示出來message
         tv2.setText(message);
+        imb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(calendar.this);
+                AlertDialog dialog = new AlertDialog.Builder(calendar.this)
+                        .setTitle("備註:")
+                        .setView(input)
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String inputName = input.getText().toString();
+                                Log.d("Main", "成功加入");
+                                tv3.setText("#"+inputName);
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("Main", "click cancel");
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+        imb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(calendar.this);
+                AlertDialog dialog = new AlertDialog.Builder(calendar.this)
+                        .setTitle("備註:")
+                        .setView(input)
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String inputName = input.getText().toString();
+                                Log.d("Main", "成功加入");
+                                tv4.setText("#"+inputName);
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("Main", "click cancel");
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+        imb3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(calendar.this);
+                AlertDialog dialog = new AlertDialog.Builder(calendar.this)
+                        .setTitle("備註:")
+                        .setView(input)
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String inputName = input.getText().toString();
+                                Log.d("Main", "成功加入");
+                                tv5.setText("#"+inputName);
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("Main", "click cancel");
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+        imb4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(calendar.this);
+                AlertDialog dialog = new AlertDialog.Builder(calendar.this)
+                        .setTitle("備註:")
+                        .setView(input)
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String inputName = input.getText().toString();
+                                Log.d("Main", "成功加入");
+                                tv6.setText("#"+inputName);
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("Main", "click cancel");
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+        imb5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(calendar.this);
+                AlertDialog dialog = new AlertDialog.Builder(calendar.this)
+                        .setTitle("備註:")
+                        .setView(input)
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String inputName = input.getText().toString();
+                                Log.d("Main", "成功加入");
+                                tv7.setText("#"+inputName);
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.d("Main", "click cancel");
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+
+    }
+    public void sendMessage(View v)
+    {
+        Intent intent = new Intent(calendar.this,calendar_memo.class);
+        //宣告一個編輯框和佈局檔案中id為edit_message的編輯框連結起來。
+        //把編輯框獲取的文字賦值給String型別的message
+        String message = tv2.getText().toString();
+        //給message起一個名字，並傳給另一個activity
+        intent.putExtra("EXTRA_MESSAGE",message);
+        //啟動意圖
+        startActivity(intent);
     }
 }
--- app2/src/main/java/com/example/hy/calendar_memo.java
+++ app2/src/main/java/com/example/hy/calendar_memo.java
@@ -5,17 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class cal_edittext extends AppCompatActivity {
+public class calendar_memo extends AppCompatActivity {
+
+    EditText ed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cal_edittext);
+        setContentView(R.layout.calendar_memo);
+
+        ed = (EditText)findViewById(R.id.edit_message1);
+
+        Intent intent = getIntent();
+        //把傳送進來的String型別的Message的值賦給新的變數message
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        //把佈局檔案中的文字框和textview連結起來
+        //在textview中顯示出來message
+        ed.setText(message);
     }
     public void sendMessage(View view)
     {
-        Intent intent = new Intent(cal_edittext.this,calendar.class);
+        Intent intent = new Intent(calendar_memo.this,calendar.class);
         //宣告一個編輯框和佈局檔案中id為edit_message的編輯框連結起來。
         EditText editText = (EditText) findViewById(R.id.edit_message1);
         //把編輯框獲取的文字賦值給String型別的message
@@ -25,4 +37,5 @@ public class cal_edittext extends AppCompatActivity {
         //啟動意圖
         startActivity(intent);
     }
+
 }
